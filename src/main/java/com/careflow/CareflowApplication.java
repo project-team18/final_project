534bc09c858f4791c749fa026bca9b5bd890f6fc
Nalla Yogesh Kumar <yoginalla04@gmail.com>
2026-07-22 14:25:01 +0530

    Configure WAR packaging for Jenkins deployment

--- src/main/java/com/careflow/CareflowApplication.java
+++ src/main/java/com/careflow/CareflowApplication.java
@@ -2,9 +2,16 @@ package com.careflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CareflowApplication {
+public class CareflowApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CareflowApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareflowApplication.class, args);
